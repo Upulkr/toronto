62460cb3eb4d85270cc4e723297578c1ed44f8e5
Upul <upulkr4@gmail.com>
2023-11-03 22:18:47 +0530

    jhu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,22 @@
+import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
+import routes from "@/routes"; 
 
-import routes from "@/routes";
-
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./pages/Header";
-
 
 function App() {
   return (
     <>
       <div className="bg-white">
-        {/* <Navbar routes={routes} /> */}
-
         <Header />
       </div>
 
       <Routes>
-        {routes.map(
-          ({ path, element }, key) =>
-            element && <Route key={key} exact path={path} element={element} />
+        {routes.map(({ path, element }, key) =>
+          element && <Route key={key} path={path} element={element} />
         )}
         <Route path="*" element={<Navigate to="/home" replace />} />
-       
       </Routes>
     </>
   );
